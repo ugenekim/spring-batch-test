f3de9ae7e0fc56d7f833eb20fe4a1a1078dd53c4
Yoo-Jin, Kim <ujkim@wizcore.co.kr>
2019-06-15 20:29:48 +0900

    Running multiple jobs in parallel (2)
    - JobLauncher : TaskExecutor -> SimpleAsyncTaskExecutor

--- src/main/java/test/ujkim/spring/batch/config/BatchConfig.java
+++ src/main/java/test/ujkim/spring/batch/config/BatchConfig.java
@@ -5,16 +5,25 @@ import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class BatchConfig {
 
+//	@Bean
+//	public JobLauncher jobLauncher(ThreadPoolTaskExecutor taskExecutor, JobRepository jobRepository) {
+//		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+//		jobLauncher.setTaskExecutor(taskExecutor);
+//		jobLauncher.setJobRepository(jobRepository);
+//		return jobLauncher;
+//	}
+	
 	@Bean
-	public JobLauncher jobLauncher(ThreadPoolTaskExecutor taskExecutor, JobRepository jobRepository) {
+	public JobLauncher jobLauncher() throws Exception {
 		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-		jobLauncher.setTaskExecutor(taskExecutor);
-		jobLauncher.setJobRepository(jobRepository);
+		jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+		jobLauncher.afterPropertiesSet();
 		return jobLauncher;
 	}
 	
